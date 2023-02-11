cedc2b860b8a9c2940b895a8324570961147f6f3
Taher Haouet <33002170+Haouet@users.noreply.github.com>
2023-02-11 22:51:49 +0100

    Update Checkout.js

--- src/components/Pages/Checkout.js
+++ src/components/Pages/Checkout.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Order from './Order';
 import { useSelector } from 'react-redux';
-const baseURL = "http://localhost:5000/api/users";
+const baseURL = "https://backend-ecommerce-exw7.onrender.com/api/";
 export default function Checkout() {
     const [users, setUsers] = useState([]);
     const [username, setUsername] = useState('');
