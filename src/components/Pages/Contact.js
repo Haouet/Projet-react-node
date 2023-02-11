5076e339d9acb3fed05c01e6622ee182b2e1ab0e
Taher Haouet <33002170+Haouet@users.noreply.github.com>
2023-02-11 22:52:34 +0100

    url mail

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-const baseURL = "http://localhost:5000/email";
+const baseURL = "https://backend-ecommerce-exw7.onrender.com/email";
 
 function Contact() {
  
