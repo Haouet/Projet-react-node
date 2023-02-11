0d03a48075dffe13362623a88e55a23e49359c78
Taher Haouet <33002170+Haouet@users.noreply.github.com>
2023-02-11 22:45:09 +0100

    url

--- API-Ecomerce/app.js
+++ API-Ecomerce/app.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://frontend-ecommerce-7xjw.onrender.com/',
 }))
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
