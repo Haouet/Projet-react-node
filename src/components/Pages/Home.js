d56c33892ce9af5740893aa847cb75572fdbf284
sedrainfo <taherhaouet@gmail.com>
2023-03-22 12:30:47 +0100

    products

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,13 +1,31 @@
 import Categorie from '../Categorie';
-
+import React, { useState, useEffect  } from 'react'
+import axios from 'axios';
 import Search from '../Search';
 import SearchElement from '../SearchElement';
 import Category from './Category';
 import Banner from './Elements/Banner';
-
+const baseURL = "https://backend-ecommerce-exw7.onrender.com/api/product";
 
 function Home() {
-    
+    const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const { data: response } = await axios.get(baseURL);
+                console.log(response.data);
+                setData(response.data);
+                
+            } catch (error) {
+                console.error(error.message);
+            }
+            setLoading(false);
+        }
+
+        fetchData();
+    }, []);
     return (
         <>
             <div className='hero'>
@@ -26,7 +44,7 @@ function Home() {
                                 {loading && <div> Loading </div>}
                                 {!loading   &&  (
                                     <div className='wrapper'>                                                                            
-                                        {data?.filter((product) => product.title.toLowerCase().includes(Search)).map(item => (
+                                        {data?.map(item => (
                                             <Product key={item._id} {...item} />
                                         ))}
                                     </div>
