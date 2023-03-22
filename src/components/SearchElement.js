183df023c6d64a158108a38788fddb08dd56ea57
sedrainfo <taherhaouet@gmail.com>
2023-03-22 13:50:10 +0100

    product css

--- src/components/SearchElement.js
+++ src/components/SearchElement.js
@@ -2,21 +2,18 @@ import React, { useState, useEffect  } from 'react'
 import axios from 'axios';
 import Product from './Product';
 const baseURL = "https://backend-ecommerce-exw7.onrender.com/api/product";
-function SearchElement({data}) {
+function SearchElement() {
     
   return (
     <section className="col-lg-12">
                 
     <div class="row">
             <div class="col-md-4 mt-2">                       
-                    {loading && <div> Loading </div>}
-                    {!loading   &&  (
+                   
                         <div className='wrapper'>                                                                            
-                            {data?.filter((product) => product.title.toLowerCase().includes(Search)).map(item => (
-                                <Product key={item._id} {...item} />
-                            ))}
+                           
                         </div>
-                    )}
+                   
                    
             </div>
         </div>
