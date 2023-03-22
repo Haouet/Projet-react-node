09749bbe352f90a9f0ce91fbea24f3eb0f23b047
sedrainfo <taherhaouet@gmail.com>
2023-03-22 12:58:01 +0100

    product css

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect  } from 'react'
 import Categorie from '../Categorie';
 import axios from 'axios';
-
+import '../Product.css'
 import Search from '../Search';
 import SearchElement from '../SearchElement';
 
