1dde362c18788256026f32fc7661526d22619227
sedrainfo <taherhaouet@gmail.com>
2023-03-23 14:19:53 +0100

    search home

--- src/components/Product.js
+++ src/components/Product.js
@@ -64,6 +64,26 @@ export default function Product({ _id, title, price, description, images
                 </button>
 
             </div>
+
+
+            <div class="col-lg-4">
+                                    <div class="product__discount__item">
+                                        <div class="product__discount__item__pic set-bg"
+                                            data-setbg="img/product/discount/pd-2.jpg">
+                                            <div class="product__discount__percent">-20%</div>
+                                            <ul class="product__item__pic__hover">
+                                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
+                                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
+                                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+                                            </ul>
+                                        </div>
+                                        <div class="product__discount__item__text">
+                                            <span>Vegetables</span>
+                                            <h5><a href="#">Vegetables’package</a></h5>
+                                            <div class="product__item__price">$30.00 <span>$36.00</span></div>
+                                        </div>
+                                    </div>
+                                </div>
         </div>
 
     )
